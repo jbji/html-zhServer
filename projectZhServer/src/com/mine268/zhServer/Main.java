4ab90d3af91504d21e2d9130d6a48102ef686f42
jbji <jbji@foxmail.com>
2022-09-08 19:17:57 +0800

    Adding Example WebServer for Reference

--- projectZhServer/src/com/mine268/zhServer/Main.java
+++ projectZhServer/src/com/mine268/zhServer/Main.java
@@ -3,7 +3,7 @@ package com.mine268.zhServer;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+        WebServer ws = new WebServer();
+        ws.start();
     }
 }
